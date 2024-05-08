6af5f97a34e964f28d85a97c8c00a96f346debe0
dongjun6343 <dongjun6343@naver.com>
2024-05-08 16:15:12 +0900

    :tada: 다형성 - 역할과 구현 예제1

--- src/main/java/com/java/basic/poly/car/CarMain0.java
+++ src/main/java/com/java/basic/poly/car/CarMain0.java
@@ -6,5 +6,17 @@ public class CarMain0 {
         K3Car k3Car = new K3Car();
         driver.setK3Car(k3Car);
         driver.driver();
+
+        // 추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null); // k3의 참조 제거
+        driver.setModel3Car(model3Car);
+        driver.driver();
+
+        /**
+         * 여기서 새로운 차량을 추가한다면 Dirver 코드를 많이 변경해야한다. -> 분기처리에서 계속 추가.
+         * 만약 운전할 수 있는 차량의 종류가 계속 늘어난다면 변경 되는 코드가 많아진다.
+         * => 역할과 구현을 분리하지 않아서 발생하는 문제이다.
+         */
     }
 }
