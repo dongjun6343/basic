086e9fa054fa283f746dd1c73fcf5de489f0615f
dongjun6343 <dongjun6343@naver.com>
2024-05-08 16:10:56 +0900

    :tada: 다형성 - 역할과 구현 예제1

---
+++ src/main/java/com/java/basic/poly/car/CarMain0.java
@@ -0,0 +1,10 @@
+package com.java.basic.poly.car;
+
+public class CarMain0 {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        K3Car k3Car = new K3Car();
+        driver.setK3Car(k3Car);
+        driver.driver();
+    }
+}
