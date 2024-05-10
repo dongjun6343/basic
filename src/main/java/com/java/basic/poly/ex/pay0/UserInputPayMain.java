a792a9fa487e9c12b73899a1b2c5b0082be483b8
dongjun6343 <dongjun6343@naver.com>
2024-05-10 11:31:48 +0900

    :recycle: 결재 시스템 사용자 입력으로 수정

---
+++ src/main/java/com/java/basic/poly/ex/pay0/UserInputPayMain.java
@@ -0,0 +1,30 @@
+package com.java.basic.poly.ex.pay0;
+
+
+import java.util.Scanner;
+
+/**
+ * 문제3: 결제 시스템 사용자 입력으로 수정
+ */
+public class UserInputPayMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PayService payService = new PayService();
+
+        while (true) {
+            System.out.print("결제 수단을 입력하세요 :: ");
+            String payOption = sc.nextLine();
+
+            if (payOption.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            System.out.print("결제 금액을 입력하세요.");
+            int amount = sc.nextInt();
+            sc.nextLine();
+
+            payService.processPay(payOption, amount);
+        }
+    }
+}
